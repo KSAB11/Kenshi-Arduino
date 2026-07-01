9416793ce6a4b9da833e51335af868605a34127d
Kenshi Sabio <kenshisabio09@gmail.com>
2026-07-01 20:44:20 +0800

    long press button turn on

---
+++ arduino-uno/longpress_button_led/longpress_button_led.ino
@@ -0,0 +1,32 @@
+unsigned long startTime = 0;
+unsigned long currentTime = 0;
+bool isPressed = false; // SWITCH OFF
+
+void setup() {
+  pinMode(13, OUTPUT); // LED
+  pinMode(12, OUTPUT); // LED
+  pinMode(2, INPUT_PULLUP); // BUTTON
+
+}
+
+void loop() {
+  if(digitalRead(2) == LOW && isPressed == false) {
+    startTime = millis();
+    isPressed = true; // PRESSED BUTTON
+  }
+
+  currentTime = millis(); // Track milliseconds 
+
+  // HOLD THE BUTTON = TURN ON
+  if (isPressed == true && (currentTime - startTime >= 1)) {
+    digitalWrite(13, HIGH);
+    digitalWrite(12, HIGH);
+  }
+
+  // LET GO OF THE BUTTON = TURN OFF
+  if(digitalRead(2) == HIGH) { 
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    isPressed = false; // SWITCH OFF
+  }
+}
